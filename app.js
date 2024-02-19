796358052252cfa9bdba377100b665f53143dfc7
boburbo <bobur20081608@gmail.com>
2024-02-19 22:43:02 +0500

    vazifa

---
+++ app.js
@@ -0,0 +1,110 @@
+console.log(1);
+console.log(2);
+
+
+let ism = 'bobur'
+let yosh = '16'
+console.log(ism,yosh);
+
+yosh = 25
+
+console.log(yosh);
+
+const familya = 'Botajonov'
+familya = 'pulatov'
+
+console.log(familya);
+
+var joy = 'fargona'
+joy = 'fargona shaxari'
+console.log(joy);
+
+let meningIsmimBu = 'bobur'
+let mening_yoshim = 24
+let = 24
+
+console.log('hello world + !');
+
+let email = 'akrorweb@gmail.com'
+console.log(email);
+
+//string concatenation
+let firstName = 'bobur'
+let lastName = 'botajonov'
+let fullNAme = ' bobur ' + ' ' + ' botajonob'
+console.log(fullNAme);
+
+//get charactes 
+console.log(fullNAme[2]);
+
+//string lengh => uzunlik 
+console.log(fullNAme.length);4
+
+
+//string metdodos
+
+console.log(fullNAme.toLowerCase);
+
+let radius = 10
+const pi = 3.14
+
+console.log(radius, pi);
+
+//moth operators + - / . . . 
+
+//conole.log(10/2)
+
+// let result = 12 % 5
+// console.log(result);
+
+// let result = pi * radius ** 2
+
+// console.log(result);
+
+// let likes = 10
+
+// let counterA = 0;
+// let counterB = 0;
+// for (let i = 0; i <text.length; i++){
+//     if (text[i] == "a") {
+//         counterA++
+//     }
+//     if (text[i] == "i") {
+//         counterB++
+//     }
+// }
+
+// console.log(counterA);
+// console.log(counterB);
+
+// let a = 5, b = 10;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log( a % b);
+// console.log(a++);
+// console.log(a);
+// console.log(++a);
+// console.log(a);
+// console.log(b--);
+// console.log(b);
+// console.log(--b);
+// console.log(b);
+
+
+// string
+// let ism = 'Bobur';
+//     let message = 'salom' + '!';
+//     console.log(message);
+// function varAndLetScoping() {
+//     var x = 1;
+  
+//     if (true) {
+//       let x = 2;
+//       console.log(x); // will print 2
+//     }
+  
+//     console.log(x); // will print 1
+//   }
+  
